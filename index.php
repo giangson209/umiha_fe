8ae99caf2ccdc94b18efba6d72af2bd17c9e6296
giangson209 <ledinhson209@gmail.com>
2023-02-22 07:47:50 +0700

    update FE

--- index.php
+++ index.php
@@ -1,5 +1,319 @@
 <?php include 'header.php';?>
-<main></main>
+<main>
+    <section class="box-banner text-center">
+        <div class="slide-banner">
+            <div class="item-slide">
+                <div class="item-banner"><div class="avr"><img src="images/banner.png" class="img-fluid" alt=""></div></div>
+            </div>
+            <div class="item-slide">
+                <div class="item-banner"><div class="avr"><img src="images/banner.png" class="img-fluid" alt=""></div></div>
+            </div>
+            <div class="item-slide">
+                <div class="item-banner"><div class="avr"><img src="images/banner.png" class="img-fluid" alt=""></div></div>
+            </div>
+        </div>
+    </section>
+    <section class="box-service">
+        <div class="container">
+            <div class="list-service">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="item-service">
+                            <div class="icon"><img src="images/srv-1.png" class="img-fluid" alt=""></div>
+                            <div class="info">
+                                <h3>Hỗ trợ Online 24/7</h3>
+                                <div class="desc">Những chiếc hộp được đóng gói cẩn thận cùng với giấy hướng dẫn sử dụng và phụ kiện đi kèm giúp bạn có được trải nghiệm tốt nhất và sản phẩm đạt hiệu quả cao nhất</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="item-service">
+                            <div class="icon"><img src="images/srv-2.png" class="img-fluid" alt=""></div>
+                            <div class="info">
+                                <h3>Vận chuyển nhanh</h3>
+                                <div class="desc">Sản phẩm của chúng tôi đem lại giải pháp cho những vấn đề bạn muốn cải thiện trên cơ thể. Đúng công dụng - đủ tác dụng - không chất dư thừa</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="item-service">
+                            <div class="icon"><img src="images/srv-3.png" class="img-fluid" alt=""></div>
+                            <div class="info">
+                                <h3>Miễn phí đổi trả</h3>
+                                <div class="desc">Luôn lắng nghe và thấu hiểu nhằm hướng tới sự hài lòng và tận hưởng dịch vụ tốt nhất, cũng như luôn đặt quyền lợi của người mua lên hàng đầu.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="item-service">
+                            <div class="icon"><img src="images/srv-4.png" class="img-fluid" alt=""></div>
+                            <div class="info">
+                                <h3>Quà tặng hấp dẫn</h3>
+                                <div class="desc">Món quà nhỏ trong mỗi đơn hàng là lời cảm ơn cho sự ủng hộ và hy vọng cho sự hài lòng của khách hàng sau mỗi lần sử dụng.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="box-product-cate">
+        <div class="container">
+            <div class="title text-center">
+                <h2>Các dòng sản phẩm</h2>
+            </div>
+            <div class="list-prd-cate">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-md-3">
+                        <div class="list-cate-prd">
+                            <ul>
+                                <li><a href="">Chăm sóc da</a></li>
+                                <li><a href="" class="active">Chăm sóc tóc</a></li>
+                                <li><a href="">Chăm sóc cơ thể</a></li>
+                                <li><a href="">Chăm sóc răng miệng </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="avr-cate text-center"><img src="images/avr-cate.png" class="img-fluid" alt=""></div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="txt-prd-cate">
+                            <h4>Lắng nghe điều cơ thể cần</h4>
+                            <div class="desc">
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh.
+                            </div>
+                            <div class="btn-main"><a href="">XEM THÊM</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="box-banner-sale">
+        <div class="container">
+            <div class="content-banner-sale">
+                <div class="row align-items-center">
+                    <div class="col-md-4">
+                        <div class="txt-sale">
+                            <h3 class="text-uppercase">bột than <br>hoạt tính</h3>
+                            <div class="btn-main"><a href="">XEM THÊM</a></div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 text-center">
+                        <div class="avr"><img src="images/avr-sale.png" class="img-fluid" alt=""></div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="slogan"><img src="images/title-banner.png" class="img-fluid" alt=""></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="box-stories">
+        <div class="container">
+            <div class="title text-center text-uppercase ">
+                <h2>Câu chuyện thương hiệu</h2>
+            </div>
+            <div class="slide-stories">
+                <div class="item-slide">
+                    <div class="item-stories text-center">
+                        <div class="avarta"><img src="images/cauchuyen.png" class="img-fluid" alt=""></div>
+                        <div class="info">
+                            <h5>Khởi đầu câu chuyện của UMIHa</h5>
+                            <div class="desc">
+                                Câu chuyện của chúng tôi bắt đầu từ năm 2015 tại vùng đất Cà Mau - nơi nguyên liệu cho sản phẩm đầu tiên được tìm thấy. Với niềm tin vào một thương hiệu đem nguyên liệu thiên nhiên phục vụ cho cuộc sống hiện đại, Umiha ra đời, mang theo mình sứ mệnh cùng bạn cải thiện những vấn đề phiền phức trên cơ thể."
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item-slide">
+                    <div class="item-stories text-center">
+                        <div class="avarta"><img src="images/cauchuyen.png" class="img-fluid" alt=""></div>
+                        <div class="info">
+                            <h5>Khởi đầu câu chuyện của UMIHa</h5>
+                            <div class="desc">
+                                Câu chuyện của chúng tôi bắt đầu từ năm 2015 tại vùng đất Cà Mau - nơi nguyên liệu cho sản phẩm đầu tiên được tìm thấy. Với niềm tin vào một thương hiệu đem nguyên liệu thiên nhiên phục vụ cho cuộc sống hiện đại, Umiha ra đời, mang theo mình sứ mệnh cùng bạn cải thiện những vấn đề phiền phức trên cơ thể."
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item-slide">
+                    <div class="item-stories text-center">
+                        <div class="avarta"><img src="images/cauchuyen.png" class="img-fluid" alt=""></div>
+                        <div class="info">
+                            <h5>Khởi đầu câu chuyện của UMIHa</h5>
+                            <div class="desc">
+                                Câu chuyện của chúng tôi bắt đầu từ năm 2015 tại vùng đất Cà Mau - nơi nguyên liệu cho sản phẩm đầu tiên được tìm thấy. Với niềm tin vào một thương hiệu đem nguyên liệu thiên nhiên phục vụ cho cuộc sống hiện đại, Umiha ra đời, mang theo mình sứ mệnh cùng bạn cải thiện những vấn đề phiền phức trên cơ thể."
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="box-product-hot">
+        <div class="container">
+            <div class="title text-center">
+                <h2>Sản phẩm bán chạy</h2>
+            </div>
+            <div class="slide-prd-hot">
+                <?php
+                    for ($i = 1; $i <= 6; $i++) { ?>
+                        <div class="item-slide">
+                            <div class="item-prd text-center">
+                                <div class="avarta"><a href=""><img src="images/prd-2.png" class="img-fluid" alt=""></a></div>
+                                <div class="info">
+                                    <h3><a href="">Mỡ trăn U Minh Hạ (30ml) </a></h3>
+                                    <ul>
+                                        <li>30ml</li>
+                                        <li>60ml</li>
+                                    </ul>
+                                    <div class="price">
+                                        <del> 145.000đ</del>
+                                        <span> 125.000đ</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php }
+                ?>
+            </div>
+        </div>
+    </section>
+    <section class="box-review">
+        <div class="container">
+            <div class="title text-center">
+                <h2>UMIHA’s review</h2>
+            </div>
+            <div class="tab-review text-center">
+                <ul>
+                    <li><a href="javascript:void(0)" class="clc-tab" data-tab="tab-1">Báo chí</a></li>
+                    <li><a href="javascript:void(0)" class="clc-tab" data-tab="tab-2">Tiktok</a></li>
+                    <li><a href="javascript:void(0)" class="clc-tab" data-tab="tab-3">Youtube</a></li>
+                </ul>
+            </div>
+            <div class="content-review">
+                <div class="tab-content" id="tab-1">
+                    <div class="slide-review">
+                        <div class="item-slide">
+                            <div class="item-review">
+                                <div class="avarta"><a href=""><img src="images/review-1.png" class="img-fluid w-100" alt=""></a></div>
+                            </div>
+                        </div>
+                        <div class="item-slide">
+                            <div class="item-review">
+                                <div class="avarta"><a href=""><img src="images/review-2.png" class="img-fluid w-100" alt=""></a></div>
+                            </div>
+                        </div>
+                        <div class="item-slide">
+                            <div class="item-review">
+                                <div class="avarta"><a href=""><img src="images/review-1.png" class="img-fluid w-100" alt=""></a></div>
+                            </div>
+                        </div>
+                        <div class="item-slide">
+                            <div class="item-review">
+                                <div class="avarta"><a href=""><img src="images/review-2.png" class="img-fluid w-100" alt=""></a></div>
+                            </div>
+                        </div>
+                        <div class="item-slide">
+                            <div class="item-review">
+                                <div class="avarta"><a href=""><img src="images/review-1.png" class="img-fluid w-100" alt=""></a></div>
+                            </div>
+                        </div>
+                        <div class="item-slide">
+                            <div class="item-review">
+                                <div class="avarta"><a href=""><img src="images/review-2.png" class="img-fluid w-100" alt=""></a></div>
+                            </div>
+                        </div>
+                        <div class="item-slide">
+                            <div class="item-review">
+                                <div class="avarta"><a href=""><img src="images/review-1.png" class="img-fluid w-100" alt=""></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="box-blog">
+        <div class="container">
+            <div class="title text-center">
+                <h2>UMIHA’s Blog</h2>
+            </div>
+            <div class="slide-blog">
+                <div class="item-slide">
+                    <div class="item-blog">
+                        <div class="avarta"><a href=""><img src="images/blog-1.png" class="img-fluid w-100" alt=""></a></div>
+                        <div class="info">
+                            <div class="top">
+                                <div class="cate">Review sản phẩm</div>
+                                <div class="date">10/9/2022</div>
+                            </div>
+                            <h3><a href="">Nên bắt đầu chống lão hoá da từ bao nhiêu tuổi</a></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="item-slide">
+                    <div class="item-blog">
+                        <div class="avarta"><a href=""><img src="images/blog-2.png" class="img-fluid w-100" alt=""></a></div>
+                        <div class="info">
+                            <div class="top">
+                                <div class="cate">Bí quyết làm đẹp</div>
+                                <div class="date">10/9/2022</div>
+                            </div>
+                            <h3><a href="">Nên bắt đầu chống lão hoá da từ bao nhiêu tuổi</a></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="item-slide">
+                    <div class="item-blog">
+                        <div class="avarta"><a href=""><img src="images/blog-3.png" class="img-fluid w-100" alt=""></a></div>
+                        <div class="info">
+                            <div class="top">
+                                <div class="cate">Review sản phẩm</div>
+                                <div class="date">10/9/2022</div>
+                            </div>
+                            <h3><a href="">Nên bắt đầu chống lão hoá da từ bao nhiêu tuổi</a></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="item-slide">
+                    <div class="item-blog">
+                        <div class="avarta"><a href=""><img src="images/blog-2.png" class="img-fluid w-100" alt=""></a></div>
+                        <div class="info">
+                            <div class="top">
+                                <div class="cate">Bí quyết làm đẹp</div>
+                                <div class="date">10/9/2022</div>
+                            </div>
+                            <h3><a href="">Nên bắt đầu chống lão hoá da từ bao nhiêu tuổi</a></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="box-subscribe">
+        <div class="container">
+            <div class="content-subscribe">
+                <div class="row align-items-center">
+                    <div class="col-md-6">
+                        <div class="txt-subscribe">
+                            <h3>Đăng ký email nhận Khuyến mãi định kỳ</h3>
+                            <div class="frm-sub">
+                                <div class="item"><input type="text" placeholder="Nhập địa chỉ email" class="txt_field"></div>
+                                <div class="item"><input type="submit" value="Send" class="btn_field"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="avarta text-center"><img src="images/avr-sale.png" class="img-fluid" alt=""></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
 <?php include 'footer.php';?>
 
 
